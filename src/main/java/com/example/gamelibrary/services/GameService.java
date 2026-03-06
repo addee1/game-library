559f5387acdc486eeeaf26cc56c46192c1eb72e9
Adam Ottosson <addeecsgo@gmail.com>
2026-03-06 20:14:04 +0100

    Implement GameService CRUD operations

--- src/main/java/com/example/gamelibrary/services/GameService.java
+++ src/main/java/com/example/gamelibrary/services/GameService.java
@@ -1,6 +1,9 @@
 package com.example.gamelibrary.services;
 
+import com.example.gamelibrary.dtos.CreateGameDTO;
+import com.example.gamelibrary.dtos.UpdateGameDTO;
 import com.example.gamelibrary.entities.Game;
+import com.example.gamelibrary.mappers.GameMapper;
 import com.example.gamelibrary.repositories.GameRepository;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +20,31 @@ public class GameService {
 
     public List<Game> getAllGames(){
         return gameRepository.findAll();
+    }
+
+    public Game createGame(CreateGameDTO dto){
+        Game game = GameMapper.toEntity(dto);
+
+        return gameRepository.save(game);
+    }
+
+    public void deleteGame(Long id) {
+
+        Game game = getGameById(id);
+
+        gameRepository.delete(game);
+    }
+
+    public Game getGameById(Long id){
+        return gameRepository.findById(id).orElseThrow(() -> new RuntimeException("Game not found"));
+    }
+
+    public Game updateGame(Long id, UpdateGameDTO dto) {
+
+        Game game = getGameById(id);
+
+        GameMapper.updateEntity(game, dto);
+
+        return gameRepository.save(game);
     }
 }
