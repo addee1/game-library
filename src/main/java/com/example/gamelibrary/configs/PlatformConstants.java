b1148cfac9967192bd40dd7aa8d7ed9798ecb0fd
Adam Ottosson <addeecsgo@gmail.com>
2026-03-19 00:53:30 +0100

    feat: add predefined platform constants

---
+++ src/main/java/com/example/gamelibrary/configs/PlatformConstants.java
@@ -0,0 +1,13 @@
+package com.example.gamelibrary.configs;
+
+import java.util.List;
+
+public class PlatformConstants {
+
+    public static final List<String> PLATFORMS = List.of(
+            "PC",
+            "PlayStation",
+            "Xbox",
+            "Nintendo Switch"
+    );
+}
