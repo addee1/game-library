be83268c8e54e14d8c6e99f992a24b6fb9efa875
Adam Ottosson <addeecsgo@gmail.com>
2026-03-22 16:41:43 +0100

    Adds timer for success message.

--- src/main/resources/static/js/reviews.js
+++ src/main/resources/static/js/reviews.js
@@ -9,8 +9,8 @@ if(toggleBtn){
 
 
 /* STAR RATING */
-
 const stars = document.querySelectorAll("#starRating span");
+const ratingInput = document.getElementById("ratingInput");
 
 stars.forEach(star => {
 
@@ -18,18 +18,28 @@ stars.forEach(star => {
 
         const value = star.dataset.value;
 
+
+        if(ratingInput){
+            ratingInput.value = value;
+        }
+
         stars.forEach(s => {
             s.classList.remove("active");
         });
 
         stars.forEach(s => {
-
             if(s.dataset.value <= value){
                 s.classList.add("active");
             }
-
         });
 
     });
 
 });
+
+setTimeout(() => {
+    const msg = document.querySelector(".form-success");
+    if(msg){
+        msg.style.opacity = "0";
+    }
+}, 3000);
